8ed59505de68573c8c472e264ae484afe1e32af8
Spencer Wolf <spencer.wolf@gmail.com>
2026-05-14 22:20:41 -0700

    Added Prefect config to locally run pipeline on a daily basis for extraction and loading of data

--- scripts/run_pipeline.py
+++ scripts/run_pipeline.py
@@ -1,16 +1,28 @@
 import sys
 sys.path.insert(0, "/app")
 
+from prefect import flow, task
 from extract.tautulli_client import get_all_history
 from load.loader import load_records
 
-if __name__ == "__main__":
+@task
+def extract_task():
     print("Extracting from Tautulli...")
     records = get_all_history()
     print(f"Pulled {len(records)} records from API")
+    return records
 
+@task
+def load_task(records):
     print("Loading to DuckDB...")
     new_count = load_records(records)
     print(f"Loaded {new_count} new records")
 
+@flow(name="wolf-tracks")
+def pipeline():
+    records = extract_task()
+    load_task(records)
     print("Pipeline Complete!")
+
+if __name__ == "__main__":
+    pipeline()
